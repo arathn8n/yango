e66b92fd49e146a2ecd157a80a4065323304a5d8
arath abello <arathabello60@gmail.com>
2026-07-18 13:44:07 -0400

    comenzando homeBlog

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -6,6 +6,7 @@ import HomeServices from './HomeServices'
 import HomeCompany from './HomeCompany'
 import HomeMission from './HomeMission'
 import HomeOpinion from './HomeOpinion'
+import HomeBlog from './HomeBlog'
 
 const Home = () => {
     return (
@@ -32,6 +33,7 @@ const Home = () => {
             <HomeCompany />
             <HomeMission />
             <HomeOpinion />
+            <HomeBlog />
 
         </>
     )
